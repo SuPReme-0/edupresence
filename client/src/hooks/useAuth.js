740368591f0688a3b85aa001496b4396af7e4a0a
SuPReme-0 <92906276+SuPReme-0@users.noreply.github.com>
2025-09-18 01:37:32 +0530

    Update useAuth.js

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
-import { supabase } from '../utils/supabaseClient';
+import supabase from "../services/supabaseClient";
+
 
 export const useAuth = () => {
   const [user, setUser] = useState(null);
